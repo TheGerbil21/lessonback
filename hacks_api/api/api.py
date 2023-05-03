96d5140581819105a61ba40a8ddf20f6e04d4e8e
jeffrey <leejeffreysc@gmail.com>
2023-05-03 10:09:05 -0700

    backend changes

--- hacks_api/api/api.py
+++ hacks_api/api/api.py
@@ -1,18 +1,99 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
 from flask_restful import Api, Resource, reqparse
 from .. import db
-from ..model.model import # class from model
+from ..model.model import Computer
 
-example_bp = Blueprint("leaderboards", __name__)
-example_api = Api(example_bp)
+computer_bp = Blueprint("computers", __name__)
+computer_api = Api(computer_bp)
+
+def get_name_list():
+    names_list = [[name._name] for name in Computer.query.all()]
+    return names_list
+
+def find_by_name(name):
+    names = Computer.query.filter_by(_name=name).all()
+    return names[0]
 
 
-class ExampleAPI(Resource):
-# make sure to have all of CRUD made here
-# make a second api for images as well, in a different file
+class ComputerAPI(Resource):
+    def get(self):
+        name = request.get_json().get("name")
+        print(name, "name")
+        name = find_by_name(name)
+        if name:
+            return name.to_dict()
+        return {"message": name}, 404
 
-class ExampleListAPI(Resource):
-# edit the links below in order to 
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument("name", required=True, type=str)
+        parser.add_argument("year", required=True, type=int)
+        parser.add_argument("age", required=True, type=int)
+        args = parser.parse_args()
 
-example_api.add_resource(ExampleAPI, "/data-points")
-example_api.add_resource(ExampleListAPI, "/database")
+        computer = Computer(args["name"], args["year"],
+                                  args["age"])
+        try:
+            db.session.add(computer)
+            db.session.commit()
+            return computer.to_dict(), 201
+        except Exception as e:
+            db.session.rollback()
+            return {"message": f"server error: {e}"}, 500
+
+    def put(self):
+        name = request.get_json().get("name")
+        print(name, "name")
+
+        try:
+            name = find_by_name(name)
+            if name:
+                name.year = int(request.get_json().get("year"))
+                name.age = int(request.get_json().get("age"))
+                db.session.commit()
+                return name.to_dict(), 201
+            else:
+                return {"message": "computer not found"}, 404
+        except Exception as e:
+            db.session.rollback()
+            return {"message": f"server error: {e}"}, 500
+
+    def delete(self):
+        name = request.get_json().get("name")
+        print(name, "name")
+
+        try:
+            name = find_by_name(name)
+            if name:
+                db.session.delete(name)
+                db.session.commit()
+                return name.to_dict()
+            else:
+                return {"message": "computer not found"}, 404
+        except Exception as e:
+            db.session.rollback()
+            return {"message": f"server error: {e}"}, 500
+
+
+class ComputerListAPI(Resource):
+    def get(self):
+        try:
+            computers = db.session.query(Computer).all()
+            return [computer.to_dict() for computer in computers]
+        except Exception as e:
+            db.session.rollback()
+            return {"message": f"server error: {e}"}, 500
+
+    def delete(self):
+        try:
+            db.session.query(Computer).delete()
+            db.session.commit()
+            return []
+        except Exception as e:
+            db.session.rollback()
+            return {"message": f"server error: {e}"}, 500
+        
+
+
+computer_api.add_resource(ComputerAPI, "/computer")
+computer_api.add_resource(ComputerListAPI, "/computerList")
--- hacks_api/model/model.py
+++ hacks_api/model/model.py
@@ -3,6 +3,175 @@ from .. import db
 from sqlalchemy.exc import IntegrityError
 import json
 
+# 
+# Computer DB class that maps computer SQL table 
+#
+class Computer(db.Model):
+    __tablename__ = "computer"
+
+    # 
+    # DB columns for brand, year, and age
+    #    
+    id = Column(Integer, primary_key=True)
+    _name = Column(String(255), unique=True, nullable=False)
+    _year = Column(Integer, nullable=False)
+    _age = Column(Integer, nullable=False)
+
+    # 
+    # DB class constructor 
+    #
+    def __init__(self, name, year, age):
+        self._name = name
+        self._year = year
+        self._age = age
+
+    # 
+    # DB class string representation of an object
+    #
+    def __repr__(self):
+        return "<Student(id='%s', name='%s', year='%s', age='%s'>" % (
+            self.id,
+            self.name,
+            self.year,
+            self.age,
+        )
+
+    # 
+    # Returns computer brand
+    #    
+    @property
+    def name(self):
+        return self._name
+
+    # 
+    # Sets computer brand
+    #        
+    @name.setter
+    def name(self, value):
+        self._name = value
+
+    # 
+    # checks computer brand
+    #            
+    def is_name(self, name):
+        return self._name == name
+
+    # 
+    # Returns computer year
+    #        
+    @property
+    def year(self):
+        return self._year
+
+    # 
+    # Sets computer year
+    #        
+    @year.setter
+    def year(self, value):
+        self._year = value
+
+    # 
+    # Sets computer age
+    #            
+    @property
+    def age(self):
+        return self._age
+
+    # 
+    # Sets computer age
+    #        
+    @age.setter
+    def age(self, value):
+        self._age = value
+
+  
+
+    # 
+    # Converts data to dictionary
+    #            
+    def to_dict(self):
+        return {"id": self.id, "name": self.name, "year": self.year, "age": self._age}
+
+    # 
+    # Converts Leaderboard to string values
+    #                
+    def __str__(self):
+        return json.dumps(self.read())
+
+    # 
+    # Creates database
+    #                
+    def create(self):
+        try:
+            db.session.add(self)
+            db.session.commit()
+            return self
+        except IntegrityError:
+            db.session.remove()
+            return None
+    # 
+    # Returns brand value pairs
+    #            
+    def read(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "year": self.year,
+            "age": self.age,
+        }
+
+    # 
+    # Updates DB rows for computer data
+    #                
+    def update(self, name="", year="", age=""):
+        """only updates values with length"""
+        if len(name) > 0:
+            self.name = name
+        if len(year) > 0:
+            self.year = year
+        if len(age) > 0:
+            self.age = age
+        db.session.add(self)
+        db.session.commit()
+        return self
+
+    # 
+    # Deletes row from the DB
+    #                
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+        return None
+
+    # 
+    # Initializes DB with test data
+    #            
+def init_computers():
+    """Create database and tables"""
+    # db.create_all()
+    """Tester data for table"""
+    c1 = Computer(name="Apple", year=2004,
+                     age=19)
+    c2 = Computer(name="DELL", year=2008,
+                     age=15)
+    c3 = Computer(name="Lenovo", year=2016,
+                     age=7)
+    c4 = Computer(name="Samsung", year=2000,
+                     age=23)
+    computers = [c1, c2, c3, c4]
+
+    """Builds sample user/note(s) data"""
+    for c in computers:
+        try:
+            '''add user to table'''
+            object = c.create()
+            print(f"Created new uid {object.name}")
+            db.session.add(c)
+            db.session.commit()
+        except:
+            '''fails with bad or duplicate data'''
+            print(f"Records exist uid {c.name}, or error.")
+
 # place your model code here
 # you can use the code we showed in our lesson as an example
 
